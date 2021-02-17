fd43313164da116979f1cb8a3f2b7ca67a80ff8c
Danil Kuibida <60036237+sviuz@users.noreply.github.com>
2021-02-17 17:39:49 +0200

    Added player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed = 5f;
+
+    public Rigidbody2D rd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
