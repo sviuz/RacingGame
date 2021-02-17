6241148abd620f6def262ad42fa54217ee44440f
Danil Kuibida <60036237+sviuz@users.noreply.github.com>
2021-02-17 17:32:12 +0200

    Added track

---
+++ Assets/Scripts/MoveTrack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+using UnityEditorInternal;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class MoveTrack : MonoBehaviour
+{
+    public float speed;
+    //public GameObject track;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Vector2 offset = new Vector2(0, Time.time + speed/2);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
